1b89f0d0cf841e39697759d9ae34be1118dd91cb
Shahan Khan <shahan.ali.khan.fiem@gmail.com>
2025-04-07 21:01:27 +0530

    Redux setup - Commit to see how it is done.

--- Professional/1-fast-react-pizza/src/features/user/Username.jsx
+++ Professional/1-fast-react-pizza/src/features/user/Username.jsx
@@ -1,5 +1,13 @@
+import { useSelector } from "react-redux";
+
 function Username() {
-    return <div className="hidden md:block text-sm font-semibold">Shahan</div>;
+    const username = useSelector((state) => state.user.username);
+    if (!username) {
+        return null;
+    }
+    return (
+        <div className="hidden md:block text-sm font-semibold">{username}</div>
+    );
 }
 
 export default Username;
